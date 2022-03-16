9e0ad42b9dd0ebea10b6116a1197366284b82403
Migheli <98918370+Migheli@users.noreply.github.com>
2022-03-16 23:07:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import os
 import requests
+import time
 import telegram
 
 
 def main():
-    chat_id = os.getenv('CHAT_ID')
+    chat_id = os.getenv('TELEGRAM_CHAT_ID')
     bot = telegram.Bot(token=os.getenv('TELEGRAM_BOT_TOKEN'))
     headers = {'Authorization': f'Token {os.getenv("DEVMAN_TOKEN")}'}
     params = {'timestamp': None}
     while True:
         try:
-            response = requests.get('https://dvmn.org/api/long_polling/', headers=headers, params=params, timeout=6)
+            response = requests.get('https://dvmn.org/api/long_polling/', headers=headers, params=params, timeout=60)
             response.raise_for_status()
-            devman_response_data = response.json()
-            if devman_response_data['status'] == 'found':
-                params['timestamp'] = devman_response_data['last_attempt_timestamp']
-                lesson_title = devman_response_data['new_attempts'][0]['lesson_title']
-                lesson_url = devman_response_data['new_attempts'][0]['lesson_url']
-                bot.send_message(chat_id=chat_id, text=f"""У Вас проверили работу "{lesson_title}".
-                                                           Cсылка на работу: {lesson_url}""")
+            devman_api_response = response.json()
+            if devman_api_response['status'] == 'found':
+                params['timestamp'] = devman_api_response['last_attempt_timestamp']
+                last_attempt = devman_api_response['new_attempts'][0]
+                lesson_title, lesson_url = last_attempt['lesson_title'], last_attempt['lesson_url']
+                bot.send_message(chat_id=chat_id, text=
+                f'У Вас проверили работу "{lesson_title}". Cсылка на работу: {lesson_url}')
             else:
-                params['timestamp'] = devman_response_data['timestamp_to_request']
+                params['timestamp'] = devman_api_response['timestamp_to_request']
         except requests.exceptions.ReadTimeout:
             pass
         except requests.exceptions.ConnectionError:
+            time.sleep(30)
             pass
 
 
