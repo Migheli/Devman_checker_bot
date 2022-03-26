c986b570eed2dcf409029a8d0b11d848dd4d3fec
Migheli <98918370+Migheli@users.noreply.github.com>
2022-03-26 10:51:26 +0300

    код оптимизирован

--- main.py
+++ main.py
@@ -18,37 +18,37 @@ class TelegramLogsHandler(logging.Handler):
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG, format="%(process)d %(levelname)s %(asctime)s %(message)s")
+    bot = telegram.Bot(token=os.getenv('TELEGRAM_BOT_TOKEN'))
+    chat_id = os.getenv('TELEGRAM_CHAT_ID')
+    logging.basicConfig(level=logging.ERROR, format="%(levelname)s %(asctime)s %(message)s")
     logger = logging.getLogger('database')
     logger.setLevel(logging.DEBUG)
-    chat_id = os.getenv('TELEGRAM_CHAT_ID')
+    logger.addHandler(TelegramLogsHandler(bot, chat_id))
+    headers = {'Authorization': f'Token {os.getenv("DEVMAN_TOKEN")}'}
+    params = {'timestamp': None}
     while True:
-        bot = telegram.Bot(token=os.getenv('TELEGRAM_BOT_TOKEN'))
-        logger.addHandler(TelegramLogsHandler(bot, chat_id))
-        logger.debug('Бот успешно запущен')
+        logger.debug(f'Бот успешно запущен')
         try:
-            headers = {'Authorization': f'Token {os.getenv("DEVMAN_TOKEN")}'}
-            params = {'timestamp': None}
-            while True:
-                try:
-                    response = requests.get('https://dvmn.org/api/long_polling/', headers=headers, params=params, timeout=60)
-                    response.raise_for_status()
-                    devman_api_response = response.json()
-                    if devman_api_response['status'] == 'found':
-                        params['timestamp'] = devman_api_response['last_attempt_timestamp']
-                        last_attempt = devman_api_response['new_attempts'][0]
-                        lesson_title, lesson_url = last_attempt['lesson_title'], last_attempt['lesson_url']
-                        bot.send_message(chat_id=chat_id, text=
+            try:
+                response = requests.get('https://dvmn.org/api/long_polling/', headers=headers, params=params, timeout=60)
+                response.raise_for_status()
+                devman_api_response = response.json()
+                if devman_api_response['status'] == 'found':
+                    params['timestamp'] = devman_api_response['last_attempt_timestamp']
+                    last_attempt = devman_api_response['new_attempts'][0]
+                    lesson_title, lesson_url = last_attempt['lesson_title'], last_attempt['lesson_url']
+                    bot.send_message(chat_id=chat_id, text=
                         f'У Вас проверили работу "{lesson_title}". Cсылка на работу: {lesson_url}')
-                    else:
-                        params['timestamp'] = devman_api_response['timestamp_to_request']
-                except requests.exceptions.ReadTimeout:
-                    pass
-                except requests.exceptions.ConnectionError:
-                    time.sleep(30)
-                    pass
-        except Exception as error:
-            logger.debug(f'{error.message}')
+                else:
+                    logger.debug(f'Получен ответ от Девмана: нет проверенных работ')
+                    params['timestamp'] = devman_api_response['timestamp_to_request']
+            except requests.exceptions.ReadTimeout:
+                pass
+            except requests.exceptions.ConnectionError:
+                time.sleep(30)
+        except Exception as err:
+            logger.error(f'Бот упал со следующей ошибкой:')
+            logger.error(err, exc_info=True)
 
 
 if __name__ == '__main__':
